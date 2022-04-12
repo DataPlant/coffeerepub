94b035e798ac70790ce3e6d6eb7b690b2011f470
D.K Nam <Dataplant@outlook.com>
2022-04-12 19:21:22 -0400

    controller for projects being routed. Not finished

--- controllers/coffeecontroller.js
+++ controllers/coffeecontroller.js
@@ -1,3 +1,39 @@
 const router = require('express').Router();
 const db = require('../models/index.js');
 
+router.get('/', (req, res) => {
+    db.Projects.find({})
+    .populate('languages')
+    .populate('libraries')
+    .exec((err, foundProjects) => {
+        if (err) return console.log(err)
+        res.json(foundProjects)
+        console.log('foundProjects check')
+    })
+})
+
+router.get('/:id', (req, res) => {
+    db.Projects.findById(req.params.id)
+    .populate('languages')
+    .populate('libraries')
+    .exec((err, foundProject) => {
+        if (err) return console.log(err)
+        res.json(foundProject)
+    })
+})
+
+router.post('/', (req, res) => {
+    db.Projects.create(req.body, (err, createdProject) => {
+        if (err) return console.log(err)
+        res.json(createdProject)
+    })
+})
+
+router.put('/:id', (req, res) => {
+    db.Projects.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new: true},
+        
+    )
+})
